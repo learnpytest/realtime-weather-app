c4d1972654316b906c9a650febf6e023c75d407c
RachelChen <iceage91@gmail.com>
2022-01-19 18:55:42 +0800

    add serviceWorkerRegistration

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import "normalize.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,8 +13,8 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
